a1740bf7a208d951bc33378ca2a277ddc220e1a1
Tatu Pesonen <pannariiz@gmail.com>
2020-08-12 11:13:31 +0300

    Wrap in IIFE

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 const safkat = require("../lib/safkat");
-
 //Swap this for your school's URL, default is KKTavastia
 const URL = "https://www.jamix.fi/ruokalistat/?anro=97111&k=1&mt=2";
-safkat.start(URL);
+
+; (async function main() {
+    let ruokalista = await safkat.get(URL);
+    //Optional function for drawing the menu
+    safkat.print(ruokalista);
+})()
